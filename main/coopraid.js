de5002b9d4d2a3fcbc3aa7614b4b3536103c8122
ljrscls <446811346@qq.com>
2017-11-22 19:41:45 +0800

    bug fixed

--- main/coopraid.js
+++ main/coopraid.js
@@ -35,7 +35,9 @@ watchElement(".btn-quest-start.multi.se-quest-start.onm-tc-gbf", () => {
 let _isAttack = false
 watchElement(".btn-attack-start.display-on", () => {
     if (!_coopRun()) return
-    if (state.coop_script.last && !_isAttack) return
+    if (state.coop_script.last && !_isAttack) {
+        return
+    }
     log("点击攻击")
     _isAttack = false
     pressElement(".btn-attack-start.display-on")
@@ -60,37 +62,18 @@ listenAjax(data => {
     }
 })
 
-// var prepareAttack = null
-// listenWebsocketMessage((data) => {
-//     if (!state.coop.run()) return
-//     log("listen websocket message", data.data)
-//     const str = data.data
-//     const p1 = str.indexOf("point"), p2 = str.indexOf("point", p1+1), hpp = str.indexOf("boss1_hp")
-//     const point1 = parseInt(str.slice(p1 + 8, str.indexOf('"', p1 + 8)))
-//     const point2 = parseInt(str.slice(p2 + 8, str.indexOf('"', p2 + 8)))
-//     const hp = parseInt(str.slice(str.indexOf(":", hpp) + 1, str.indexOf(",", hpp)))
-//     log("battle point", point1, point2, "hp = ", hp)
-//     if (point1 > 0 || point2 > 0) {
-//         if (prepareAttack) {
-//             prepareAttack()
-//             prepareAttack = null
-//         }
-//     }
-//     if (point1 > 0 && point2 > 0 && hp === 0) {
-//         if (!state.coop.run()) return
-//         log("刷新至结果"); 
-//         setTimeout(() => {
-//             if (!state.coop.run()) return
-//             window.location.href = "/#coopraid"
-//             // TODO： 检验第二次刷新
-//             waitForElementToExist(".prt-result-head", function () {
-//                 if (!state.coop.run()) return
-//                 log("刷新至共斗");
-//                 window.location.href = "/#coopraid"
-//             }, true)
-//         }, 300)
-//     }
-// })
+listenWebSocket((data) => {
+    if (!state.coop.run()) return
+    log("listen websocket message", data.data)
+    const str = data.data
+    const p1 = str.indexOf("point"), p2 = str.indexOf("point", p1+1)
+    const point1 = parseInt(str.slice(p1 + 8, str.indexOf('"', p1 + 8)))
+    const point2 = parseInt(str.slice(p2 + 8, str.indexOf('"', p2 + 8)))
+    log("battle point", point1, point2)
+    if (point1 > 0 || point2 > 0) {
+        _isAttack = true
+    }
+})
 // var waitPress = false
 // waitForElementToExist(".btn-attack-start.display-on", function () {
 //     if (!state.coop.run()) return
